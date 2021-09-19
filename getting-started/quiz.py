0e092e7c7f95b08e4a5ff910789b028ea7a8c94f
Kasai Koji <80010105+StreamWest-1629@users.noreply.github.com>
2021-09-19 11:46:22 +0900

    ファイルを生成する

--- getting-started/quiz.py
+++ getting-started/quiz.py
@@ -3,6 +3,8 @@ import random
 nQuiz = 10
 data = []
 
+f = open(file="out.txt", mode='w')
+
 for i in range(nQuiz):
     
     # make quiz
@@ -34,3 +36,4 @@ for i in range(nQuiz):
 for i in range(nQuiz):
     first, operator, second, pyAns, cppAns = data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]
     print(str(first)+" "+operator+" "+str(second)+" = (python: "+str(pyAns)+", cpp: "+cppAns+")")
+    print(str(first)+" "+operator+" "+str(second)+" = (python: "+str(pyAns)+", cpp: "+cppAns+")", file=f)
